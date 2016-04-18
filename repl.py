bb3700bce3401a82f771ada45ee7b9dd0813b449
williumchen <wichen@g.hmc.edu>
2016-04-17 19:30:42 -0700

    added help options, working undo

--- repl.py
+++ repl.py
@@ -7,48 +7,70 @@ class Commands(Cmd):
 	prompt = '(py-DJ)'
 	file = None
 	curr_song = None
+	song_history = []
 
 	def do_load(self, arg):
-		Commands.curr_song = load_song(arg)
+		self.curr_song = load_song(arg)
+		self.song_history.append(self.curr_song)
 		print "Successfully loaded " + arg
+	def help_load(self):
+		print "Loads a song to be editted"
+
+	def do_play(self, s):
+		# TODO: add keyboard exception
+		playSample(self.curr_song)
+	def help_play(self):
+		print "Plays the song currently being editted"
+
+	def do_save(self, arg):
+		self.curr_song.export(arg, "wav")
+	def help_save(self):
+		print "Exports any changes to the current song to a file name of your choice"
+
+	def do_undo(self, arg):
+		self.curr_song = self.song_history[-2]
+	def help_undo(self):
+		print "Undoes the most recent change"
 
 	def default(self, line):
 		temp = parse(line)
 		if temp.effect == "volume":
-			Commands.curr_song = volume(Commands.curr_song, temp.action, temp.value)
+			self.curr_song = volume(self.curr_song, temp.action, temp.value)
+		self.song_history.append(self.curr_song)
+	
+	# Record and playback commands
+	def do_record(self, arg):
+		print 'Save future commands to a filename'
+		self.file = open(arg, 'w')
 
-	def do_play(self, s):
-		# TODO: add keyboard exception
-		playSample(Commands.curr_song)
+	def do_playback(self,arg):
+		print 'Playback commands from a file'
+		self.close()
+		with open(arg) as f:
+			self.cmdqueue.extend(f.read().splitlanes())
 
-	# Exiting the shell
+	def precmd(self, line):
+		line = line.lower()
+		if self.file and 'playback' not in line:
+			print(line, self.file)
+		return line
+
+	def close(self):
+		if self.file:
+			self.file.close()
+			self.file = None
+
+    # Exiting the shell
 	def can_exit(self):
 		return True
+	
 	def do_exit(self, s):
 		return True
+	
 	def help_exit(self):
 		print "Exit the interpreter."
 	do_EOF = do_exit
 	help_EOF= help_exit	
-
-	# Record and playback
-    # def do_record(self, arg):
-    #     'Save future commands to filename:  RECORD rose.cmd'
-    #     self.file = open(arg, 'w')
-    # def do_playback(self, arg):
-    #     'Playback commands from a file:  PLAYBACK rose.cmd'
-    #     self.close()
-    #     with open(arg) as f:
-    #         self.cmdqueue.extend(f.read().splitlines())
-    # def precmd(self, line):
-    #     line = line.lower()
-    #     if self.file and 'playback' not in line:
-    #         print(line, file=self.file)
-    #     return line
-    # def close(self):
-    #     if self.file:
-    #         self.file.close()
-    #         self.file = None
 
 if __name__ == '__main__':
 	Commands().cmdloop()
