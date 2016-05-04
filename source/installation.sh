3f5d498835aab6524b67d780349c93c4eea242d6
williumchen <wichen@g.hmc.edu>
2016-05-04 14:09:35 -0700

    fixed bashscript

--- source/installation.sh
+++ source/installation.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 echo "Installing necessary packages"
+# Install homebrew to install 
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install portaudio
 pip install pyparsing
 pip install pydub
 pip install pyaudio
+exit
